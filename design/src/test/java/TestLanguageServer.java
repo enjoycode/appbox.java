d71accff62d5b6049f019efb1224290e48e48f11
enjoycode <enjoycode@icloud.com>
2020-08-07 10:21:41 +0800

    [design] init design module.

---
+++ design/src/test/java/TestLanguageServer.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestLanguageServer {
+
+    @Test
+    public void testSymbols() {
+
+    }
+}
