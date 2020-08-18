3397a2613a7eeb46326fb230b27a6ffc9976d54e
enjoycode <enjoycode@icloud.com>
2020-08-18 12:56:42 +0800

    [store] test begin and end transaction.

--- host/src/test/java/TestSysStore.java
+++ host/src/test/java/TestSysStore.java
@@ -1,11 +1,15 @@
 import appbox.core.runtime.RuntimeContext;
 import appbox.server.channel.SharedMemoryChannel;
+import appbox.server.channel.messages.KVBeginTxnRequire;
+import appbox.server.channel.messages.KVEndTxnRequire;
 import appbox.server.channel.messages.KVInsertRequire;
 import appbox.server.runtime.HostRuntimeContext;
 import appbox.store.SysStoreApi;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
@@ -35,9 +39,25 @@ public class TestSysStore {
     public void testKVInsertCommand() throws ExecutionException, InterruptedException {
         var cmd = new KVInsertRequire();
         cmd.raftGroupId = 0;
-        cmd.dataCF = -1;
+        cmd.dataCF      = -1;
 
         var fut = SysStoreApi.execKVInsertAsync(cmd);
         var res = fut.get();
     }
+
+    @Test
+    public void testTxnBeginAndEnd() throws ExecutionException, InterruptedException {
+        var req1 = new KVBeginTxnRequire();
+        var fut1 = SysStoreApi.beginTxnAsync();
+        var res1 = fut1.get();
+        assertEquals(0, res1.errorCode);
+
+        var req2 = new KVEndTxnRequire();
+        req2.txnId.copyFrom(res1.txnId);
+        req2.action = 1;
+        var fut2 = SysStoreApi.endTxnAsync(req2);
+        var res2 = fut2.get();
+        assertEquals(0, res2.errorCode);
+    }
+
 }
