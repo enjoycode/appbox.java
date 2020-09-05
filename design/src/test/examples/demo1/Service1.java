7d038849f540aa9134edb40ac1dafaae76288f5c
enjoycode <enjoycode@icloud.com>
2020-09-05 13:39:44 +0800

    [design] test LanguageServer.

--- design/src/test/examples/demo1/Service1.java
+++ design/src/test/examples/demo1/Service1.java
@@ -1,6 +1,6 @@
 
 public class Main {
     public void sayHello() {
-
+        aa
     }
 }
--- design/src/test/java/MockLanguageClient.java
+++ design/src/test/java/MockLanguageClient.java
@@ -6,21 +6,22 @@ import org.javacs.lsp.ShowMessageParams;
 public class MockLanguageClient implements LanguageClient {
     @Override
     public void publishDiagnostics(PublishDiagnosticsParams publishDiagnosticsParams) {
-
+        System.out.printf("MockClient.publishDiagnostics: %s%n", publishDiagnosticsParams.uri.toString());
     }
 
     @Override
     public void showMessage(ShowMessageParams showMessageParams) {
-
+        System.out.printf("MockClient.showMeesage: %d %s%n",
+                showMessageParams.type, showMessageParams.message);
     }
 
     @Override
     public void registerCapability(String s, JsonElement jsonElement) {
-
+        System.out.printf("MockClient.registerCapability: %s: %s%n", s, jsonElement.toString());
     }
 
     @Override
     public void customNotification(String s, JsonElement jsonElement) {
-
+        System.out.printf("MockClient.customNotification: %s: %s%n", s, jsonElement.toString());
     }
 }
