29267e0e5d2658ff0ab326310a483c2d84a2faa6
enjoycode <enjoycode@icloud.com>
2020-09-05 15:35:28 +0800

    [design] test JavaParser.

--- design/src/test/examples/demo1/Service1.java
+++ design/src/test/examples/demo1/Service1.java
@@ -1,6 +1,10 @@
 
 public class Main {
     public void sayHello() {
-        aa
+        aa;
+    }
+
+    public int getNumber() {
+        return 1;
     }
 }
--- design/src/test/java/TestLanguageServer.java
+++ design/src/test/java/TestLanguageServer.java
@@ -1,10 +1,16 @@
 
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.symbolsolver.JavaSymbolSolver;
+import com.github.javaparser.symbolsolver.javaparsermodel.JavaParserFacade;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.ReflectionTypeSolver;
 import org.javacs.JavaLanguageServer;
 import org.javacs.lsp.InitializeParams;
 import org.javacs.lsp.TextDocumentPositionParams;
 import org.javacs.lsp.WorkspaceSymbolParams;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -23,14 +29,28 @@ public class TestLanguageServer {
         assertNotNull(symbols);
     }
 
-    //private String symbolAt(String file, int line, int character) {
-    //    var pos =
-    //            new TextDocumentPositionParams(
-    //                    new TextDocumentIdentifier(FindResource.uri(file)), new Position(line - 1, character - 1));
-    //    var result = new StringJoiner("\n");
-    //    for (var h : server.hover(pos).get().contents) {
-    //        result.add(h.value);
-    //    }
-    //    return result.toString();
-    //}
+    @Test
+    public void testParseWithError() throws IOException {
+        var demoFile = Paths.get("src/test/examples/demo1/Service1.java").normalize();
+        try {
+            var ts = new ReflectionTypeSolver();
+            var ss = new JavaSymbolSolver(ts);
+            var parser = new JavaParser();
+            parser.getParserConfiguration().setSymbolResolver(ss);
+
+
+            //var cu =  StaticJavaParser.parse(demoFile);
+            var result = parser.parse(demoFile);
+            var cu = result.getResult().get();
+            //var type = JavaParserFacade.get(ts).getType(cu.getChildNodes().get(0).getChildNodes().get(2));
+            //assertNotNull(cu);
+            //
+            //var ts = new ReflectionTypeSolver();
+            //var ss = new JavaSymbolSolver(ts);
+            //ss.inject(cu);
+            System.out.println("aa");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
