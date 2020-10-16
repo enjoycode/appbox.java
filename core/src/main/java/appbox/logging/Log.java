12a45b7d40ed20f3cc3a07f5b6ed7276a51d6731
enjoycode <enjoycode@icloud.com>
2020-10-16 17:13:25 +0800

    [log] 完善控制台输出信息.

--- core/src/main/java/appbox/logging/Log.java
+++ core/src/main/java/appbox/logging/Log.java
@@ -2,20 +2,30 @@ package appbox.logging;
 
 //TODO: impl it
 
+import java.time.LocalDateTime;
+
 public final class Log {
     public static void debug(String msg) {
-        System.out.println(msg);
+        log("\33[34m[D", msg);
     }
 
     public static void info(String msg) {
-        System.out.println(msg);
+        log("\33[32m[I", msg);
     }
 
     public static void warn(String msg) {
-        System.out.println(msg);
+        log("\33[33m[W", msg);
     }
 
     public static void error(String msg) {
-        System.out.println(msg);
+        log("\33[31m[E", msg);
+    }
+
+    private static void log(String level, String msg) {
+        var f = StackWalker.getInstance(/*StackWalker.Option.RETAIN_CLASS_REFERENCE*/)
+                .walk(s -> s.skip(2).limit(1).findFirst())
+                .get();
+        System.out.format("%s%s %s:%d]:\33[0m %s\n", level, LocalDateTime.now().toString(),
+                f.getFileName(), f.getLineNumber(), msg);
     }
 }
