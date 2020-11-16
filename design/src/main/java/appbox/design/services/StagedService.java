c26aa9d3411e2e01d85c4626dde9a7fdf9c9e2fe
enjoycode <enjoycode@icloud.com>
2020-11-16 13:47:13 +0800

    fix StagedService

--- design/src/main/java/appbox/design/services/StagedService.java
+++ design/src/main/java/appbox/design/services/StagedService.java
@@ -5,17 +5,12 @@ import appbox.logging.Log;
 import appbox.model.EntityModel;
 import appbox.runtime.RuntimeContext;
 import appbox.store.EntityStore;
-import appbox.store.KVTransaction;
-import appbox.store.ServiceCode;
-import appbox.store.query.SqlQuery;
 import appbox.store.query.TableScan;
 import appbox.store.utils.ModelCodeUtil;
 import appbox.utils.IdUtil;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 
 public final class StagedService {
     enum StagedType {
@@ -54,22 +49,24 @@ public final class StagedService {
         }).thenCompose(item -> EntityStore.insertEntityAsync(item, true));
     }
 
-    private static CompletableFuture<String> loadServiceCode(long serviceModelId) throws ExecutionException, InterruptedException, IOException {
-
+    private static CompletableFuture<String> loadServiceCode(long serviceModelId) {
         var developerID = RuntimeContext.current().currentSession().leafOrgUnitId();
 
-        var q = new TableScan(IdUtil.SYS_STAGED_MODEL_ID,StagedModel.class);
+        var q = new TableScan<>(IdUtil.SYS_STAGED_MODEL_ID,StagedModel.class);
         q.where(StagedModel.TYPE.eq(StagedType.SourceCode.value)
                         .and(StagedModel.MODEL.eq(Long.toUnsignedString(serviceModelId)))
                         .and(StagedModel.DEVELOPER.eq(developerID)));
 
-        var         list        = (List<StagedModel>)q.toListAsync().get();
-        if(list.size()>0){
-            ServiceCode serviceCode =ModelCodeUtil.decodeServiceCode(list.get(0).getData());
-            return CompletableFuture.completedFuture(serviceCode.sourceCode);
-        }else{
-            return CompletableFuture.completedFuture(null);
-        }
+        return q.toListAsync().thenApply(res -> {
+            if (res == null || res.size() <= 0)
+                return null;
 
+            try {
+                return ModelCodeUtil.decodeServiceCode(res.get(0).getData()).sourceCode;
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        });
     }
 }
