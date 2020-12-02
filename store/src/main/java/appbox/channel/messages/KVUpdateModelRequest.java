7a22ce7070d4ee799472cfc3dc33e2f137ae4ad5
enjoycode <enjoycode@icloud.com>
2020-12-02 17:52:34 +0800

    [store] 实现KVUpdateRequest.

---
+++ store/src/main/java/appbox/channel/messages/KVUpdateModelRequest.java
@@ -0,0 +1,34 @@
+package appbox.channel.messages;
+
+import appbox.model.ModelBase;
+import appbox.serialization.BinSerializer;
+import appbox.store.KVTxnId;
+import appbox.store.KeyUtil;
+
+public final class KVUpdateModelRequest extends KVUpdateRequest {
+
+    private final ModelBase model;
+
+    public KVUpdateModelRequest(KVTxnId txnId, ModelBase model) {
+        super(txnId);
+
+        this.model = model;
+
+        raftGroupId   = KeyUtil.META_RAFTGROUP_ID;
+        schemaVersion = 0;
+        dataCF        = -1;
+    }
+
+    @Override
+    public void writeTo(BinSerializer bs) {
+        super.writeTo(bs);
+
+        //key
+        KeyUtil.writeModelKey(bs, model.id(), true);
+        //refs
+        bs.writeVariant(0);
+        //data
+        bs.writeByte(model.modelType().value); //注意写入模型类型信息
+        model.writeTo(bs);
+    }
+}
---
+++ store/src/main/java/appbox/channel/messages/KVUpdateRequest.java
@@ -0,0 +1,47 @@
+package appbox.channel.messages;
+
+import appbox.channel.IMessage;
+import appbox.channel.MessageType;
+import appbox.serialization.BinDeserializer;
+import appbox.serialization.BinSerializer;
+import appbox.store.KVTxnId;
+
+public abstract class KVUpdateRequest implements IMessage {
+
+    protected final KVTxnId txnId         = new KVTxnId();
+    protected       long    raftGroupId   = 0;
+    protected       int     schemaVersion = 0;
+    protected       byte    dataCF        = -1;
+    protected       boolean merge         = false;
+    protected       boolean returnExists  = false;
+
+    public KVUpdateRequest(KVTxnId txnId) {
+        this.txnId.copyFrom(txnId);
+    }
+
+    @Override
+    public final byte MessageType() {
+        return MessageType.KVUpdateRequire;
+    }
+
+    //region ====Serialization====
+    @Override
+    public void writeTo(BinSerializer bs) {
+        txnId.writeTo(bs);
+
+        bs.writeLong(raftGroupId);
+        bs.writeInt(schemaVersion);
+        bs.writeByte(dataCF);
+        bs.writeBool(merge);
+        bs.writeBool(returnExists);
+
+        //子类写入key(带长度),refs及不带长度信息的data
+    }
+
+    @Override
+    public final void readFrom(BinDeserializer bs) {
+        throw new UnsupportedOperationException();
+    }
+    //endregion
+
+}
